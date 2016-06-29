00c1025513cbc8635275db367003c6726b1fd2f6
themike123 <themike123nova@gmail.com>
2016-06-28 21:04:16 -0500

    Primera version final

--- js/app_admin.js
+++ js/app_admin.js
@@ -8,6 +8,24 @@ app.controller('glosarioController', function($scope, $http){
   $scope.diaEjercicio = JSON.parse(localStorage.getItem("diaEjercicio"));
   $scope.mi1rm = JSON.parse(localStorage.getItem("mi1rm"));
 
+
+  //Funcionalidad del boton de atrás del telefono
+  document.addEventListener("backbutton",onBackKeyDown,false);
+  function onBackKeyDown() {
+    navigator.notification.confirm(
+      '¿ Salir de la Aplicación?',
+      MetodoSalir,
+      'Aviso',
+      ['Aceptar','Cancelar']
+    );
+  }
+  function MetodoSalir(botton) {
+    if (botton==1) {
+      navigator.app.exitApp();
+    }
+  }
+  ////
+
   console.log($scope.tmb);
   //TMB Mujer = 655 + (9,6 * P) + (1,8 * A) – (4,7 * E)
   //TMB Hombre = 66 + (13,7 * P) + (5 * A) – (6,8 * E)
@@ -22,39 +40,73 @@ app.controller('glosarioController', function($scope, $http){
   //Ecuaciones de Harris Benedict – Ejemplo práctico
 
   $scope.calTasaBasal = function(){
-    var TMB =0;
-    var CalNecesarias=0;
-
-    if ($("#genero").val()==1) {
-      TMB = 66 + (13.7 *$scope.peso ) + (5 *$scope.estatura) - (6.8 *$scope.edad);
-    }else {
-      TMB=655 + (9.6 * $scope.peso) + (1.8 *$scope.estatura) - (4.7 *$scope.edad);
+    var aux = 0;
+    if ( $("#genero").val().length<1  ) {
+      aux=1;
     }
-    //Metabolismo basal
-    CalNecesarias = TMB * $("#actividad").val();
-    TMB = TMB.toFixed(2);
-    TMB = Math.round(TMB);
-    //Calorias necesarias para matener peso
-    CalNecesarias = CalNecesarias.toFixed(2);
-    CalNecesarias = Math.round(CalNecesarias);
-    //calculo de su indice de masa corporal
-    var icm=($scope.peso/($scope.estatura*$scope.estatura) ) * 10000;
-    icm = icm.toFixed(1);
-    if(typeof(Storage) !== "undefined"){
-      if (localStorage.getItem("tmb")!=null) {
-        localStorage.removeItem("tmb");
 
-      }
-        localStorage.setItem("tmb", JSON.stringify({'imc': icm,'basal':TMB ,'cal':CalNecesarias}));
-      }else{
-        console.log("No se pudo guardar la información en memoria");
-      }
+    if ( $("#peso").val().length<1) {
+      aux=1;
+    }
 
-      //$scope.icm = ($scope.peso/($scope.estatura*$scope.estatura) ) * 1000;
-    window.location.href = 'resultadoTMB.html';
+    if ( $("#estatura").val().length<1) {
+      aux=1;
+    }
+
+    if ( $("#edad").val().length<1) {
+      aux=1;
+    }
+
+    if ( $("#actividad").val().length<1) {
+      aux=1;
+    }
+
+    if (aux==0) {
+      var TMB =0;
+      var CalNecesarias=0;
+
+      if ($("#genero").val()==1) {
+        TMB = 66 + (13.7 *$scope.peso ) + (5 *$scope.estatura) - (6.8 *$scope.edad);
+      }else {
+        TMB=655 + (9.6 * $scope.peso) + (1.8 *$scope.estatura) - (4.7 *$scope.edad);
+      }
+      //Metabolismo basal
+      CalNecesarias = TMB * $("#actividad").val();
+      TMB = TMB.toFixed(2);
+      TMB = Math.round(TMB);
+      //Calorias necesarias para matener peso
+      CalNecesarias = CalNecesarias.toFixed(2);
+      CalNecesarias = Math.round(CalNecesarias);
+      //calculo de su indice de masa corporal
+      var icm=($scope.peso/($scope.estatura*$scope.estatura) ) * 10000;
+      icm = icm.toFixed(1);
+      if(typeof(Storage) !== "undefined"){
+        if (localStorage.getItem("tmb")!=null) {
+          localStorage.removeItem("tmb");
+
+        }
+          localStorage.setItem("tmb", JSON.stringify({'imc': icm,'basal':TMB ,'cal':CalNecesarias}));
+        }else{
+          console.log("No se pudo guardar la información en memoria");
+        }
+
+        //$scope.icm = ($scope.peso/($scope.estatura*$scope.estatura) ) * 1000;
+      window.location.href = 'resultadoTMB.html';
+    }else {
+      navigator.notification.vibrate(100);
+      //navigator.notification.alert('Debes seleccionar 4 dias')
+      navigator.notification.alert(
+        'Ingrese sus datos en todos los campos.',
+        alertDismised,
+        'Alerta',
+        'cerrar'
+      );
+    }
+
   };
 
   $scope.Su1RM = function () {
+
     var aux=0;
 
     if ( $("#militar").val().length<1 ) {
@@ -83,10 +135,10 @@ app.controller('glosarioController', function($scope, $http){
         'cerrar'
       );
     }else {
+      //fechas();
       if(typeof(Storage) !== "undefined"){
         if (localStorage.getItem("mi1rm")!=null) {
           localStorage.removeItem("mi1rm");
-
         }
           localStorage.setItem("mi1rm", JSON.stringify(
             {'uno':$("#militar").val() ,'dos':$("#muerto").val() ,'tres':$("#press").val() ,'cuatro': $("#sentadilla").val()}));
@@ -98,7 +150,49 @@ app.controller('glosarioController', function($scope, $http){
 
   }
 
+  $scope.Pierna = function () {
+
+    if(typeof(Storage) !== "undefined"){
+      if (localStorage.getItem("diaEjercicio")!=null) {
+        localStorage.removeItem("diaEjercicio");
+      }
+        localStorage.setItem("diaEjercicio", JSON.stringify({'dia1':'Pierna','dia2': 'Torso','dia3':'Pierna','dia4' : 'Torso' }));
+      }else{
+        console.log("No se pudo guardar la información en memoria");
+      }
+    $("#select1").text($scope.dias.dia1+':Pierna, '+$scope.dias.dia2+':Torso, '+$scope.dias.dia3+':Pierna, '+$scope.dias.dia4+':Torso');
+    navigator.notification.alert(
+      $scope.dias.dia1+':Pierna\n'+$scope.dias.dia2+':Torso\n'+$scope.dias.dia3+':Pierna\n'+$scope.dias.dia4+':Torso',
+      alertDismised,
+      'Se Seleccionó',
+      'Aceptar'
+    );
+  }
+
+  $scope.Torso = function () {
+
+    if(typeof(Storage) !== "undefined"){
+      if (localStorage.getItem("diaEjercicio")!=null) {
+        localStorage.removeItem("diaEjercicio");
+      }
+        localStorage.setItem("diaEjercicio", JSON.stringify({'dia1':'Torso','dia2': 'Pierna','dia3':'Torso','dia4' : 'Pierna' }));
+      }else{
+        console.log("No se pudo guardar la información en memoria");
+      }
+    $("#select1").text($scope.dias.dia1+':Torso, '+$scope.dias.dia2+':Pierna, '+$scope.dias.dia3+':Torso, '+$scope.dias.dia4+':Pierna');
+    navigator.notification.alert(
+      $scope.dias.dia1+':Torso\n'+$scope.dias.dia2+':Pierna\n'+$scope.dias.dia3+':Torso\n'+$scope.dias.dia4+':Pierna',
+      alertDismised,
+      'Se Seleccionó',
+      'Aceptar'
+    );
+  }
+
   $scope.PiernaTorso = function(){
+    alert("ggg")
+    //alert($scope.diaEjercicio.dia1+':'+$scope.diaEjercicio.dia2+':'+$scope.diaEjercicio.dia3+':'+$scope.diaEjercicio.dia4)
+
+    /*
     var aux=0;
 
     if ($("#diaEjercicio1").val() != 'Pierna' && $("#diaEjercicio1").val() != 'Torso' ) {
@@ -137,7 +231,7 @@ app.controller('glosarioController', function($scope, $http){
         'cerrar'
       );
     }
-
+    */
   }
 
   $scope.DiasEjercicio = function(){
@@ -220,6 +314,91 @@ app.controller('glosarioController', function($scope, $http){
   function alertDismised() {
 
   }
+
+
+    function fechas() {
+      var diaSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+      var dia = new Date();
+
+      if ( dia.getDay()==SegunDiaRetornaNumero($scope.dias.dia1) ) {
+        var Semana11 = sumarFechas(7);
+        GuardarLocal(Semana11[0]+"/"+Semana11[1]+"/"+Semana11[2]);
+        alert("Semana1:"+dia.getDate()+"/"+(dia.getMonth()+1)+"/"+dia.getFullYear() );
+
+      }else if ( dia.getDay()<SegunDiaRetornaNumero($scope.dias.dia1) ) {
+        var Semana12 = sumarFechas( SegunDiaRetornaNumero($scope.dias.dia1) - dia.getDay() );
+        GuardarLocal(Semana12[0]+"/"+Semana12[1]+"/"+Semana12[2]);
+        alert("Semana1:"+Semana12[0]+"/"+Semana12[1]+"/"+Semana12[2]);
+
+      }else if ( dia.getDay()>SegunDiaRetornaNumero($scope.dias.dia1) ) {
+        var Semana13 = sumarFechas(7-(dia.getDay()-SegunDiaRetornaNumero($scope.dias.dia1) )  );
+        GuardarLocal(Semana13[0]+"/"+Semana13[1]+"/"+Semana13[2]);
+        alert("Semana1:"+Semana13[0]+"/"+Semana13[1]+"/"+Semana13[2]);
+      }
+
+    }
+
+    function GuardarLocal(sem) {
+      if(typeof(Storage) !== "undefined"){
+        if (localStorage.getItem("Semana1")!=null) {
+          localStorage.removeItem("Semana1");
+        }
+          localStorage.setItem("Semana1", JSON.stringify({'sem1':sem}));
+        }else{
+          console.log("No se pudo guardar la información en memoria");
+        }
+    }
+
+    function SegunDiaRetornaNumero(dia) {
+      var num=-1;
+      switch (dia) {
+        case 'Domingo':
+            num = 0;
+        case 'Lunes':
+            num = 1;
+            break;
+        case 'Martes':
+            num = 2;
+            break;
+        case 'Miércoles':
+            num = 3;
+            break;
+        case 'Jueves':
+            num = 4;
+            break;
+        case 'Viernes':
+            num = 5;
+            break;
+        case 'Sabado':
+            num = 6;
+            break;
+
+          }
+          return num;
+    }
+
+
+    function sumarFechas(days) {
+      milisegundos=parseInt(35*24*60*60*1000);
+
+      fecha=new Date();
+      day=fecha.getDate();
+      // el mes es devuelto entre 0 y 11
+      month=fecha.getMonth()+1;
+      year=fecha.getFullYear();
+      //document.write("Fecha actual: "+day+"/"+month+"/"+year);
+      //Obtenemos los milisegundos desde media noche del 1/1/1970
+      tiempo=fecha.getTime();
+      //Calculamos los milisegundos sobre la fecha que hay que sumar o restar...
+      milisegundos=parseInt(days*24*60*60*1000);
+      //Modificamos la fecha actual
+      total=fecha.setTime(tiempo+milisegundos);
+      day=fecha.getDate();
+      month=fecha.getMonth()+1;
+      year=fecha.getFullYear();
+      //alert(day+"/"+month+"/"+year);
+      return Array(day,month,year);
+    }
 
 
 });
--- js/app_procedimiento.js
+++ js/app_procedimiento.js
@@ -7,73 +7,256 @@ app.controller('procedimientoController', function($scope, $http){
   $scope.dias = JSON.parse(localStorage.getItem("dias"));
   $scope.diaEjercicio = JSON.parse(localStorage.getItem("diaEjercicio"));
   $scope.mi1rm = JSON.parse(localStorage.getItem("mi1rm"));
+  $scope.semana1 =JSON.parse(localStorage.getItem("Semana1"));
+  $scope.progresion1=2.5;
+  $scope.progresion2=5;
 
+  //Funcionalidad del boton de atrás del telefono
+  document.addEventListener("backbutton",onBackKeyDown,false);
+  function onBackKeyDown() {
+    navigator.notification.confirm(
+      '¿ Salir de la Aplicación?',
+      MetodoSalir,
+      'Aviso',
+      ['Aceptar','Cancelar']
+    );
+  }
 
+  function MetodoSalir(botton) {
+    if (botton==1) {
+      navigator.app.exitApp();
+    }
+  }
+  ///
+
+  //navigator.app.exitApp();
+  //$scope.f = new Date();
+
+  //f.getDate()
+  //f.getMonth()
+  //f.getFullYear()
+
+  //var diaSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+  //diaSemana[f.getDay];
+
+  function RedondeoDecenas(num) {
+    var temp=0;
+
+    if (num>100) {
+      temp=num%100;
+    }else {
+      temp=num%10;
+    }
+
+    //Validación si no necesita redondeo
+    if (temp!=0 && temp!=5) {
+
+      if (temp<4) {
+        return (num-temp);
+      }
+
+      if (temp>6) {
+        return (num-temp)+10;
+      }
+
+      return (num-temp)+5;
+    }
+
+    //Retorno el numero ya que su modulo es cero
+    return num;
+  }
 
   $scope.init = function () {
+    //alert("Mande el 31:"+RedondeoDecenas(31));
+    //if($scope.semana1!=null || $scope.semana1!=undefined){
+    //  $('#modal1').openModal();
+    //}
+
     if ($scope.mi1rm!=null || $scope.mi1rm!=undefined) {
 
-      var cabezera = '<h3>Semana 1</h3><table class="centered"><thead><tr><th data-field="id">Lunes</th><th data-field="name">Martes</th><th data-field="name">Miercoles</th><th data-field="name">Jueves</th></tr></thead><tbody><tr><td>Militar</td><td>Peso Muerto</td><td>Press Banca</td><td>Sentadilla</td></tr><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td></tr>';
+      if ($scope.diaEjercicio.dia1=='Pierna') {
+        var cabezera = '<h3>Semana 1</h3><table class="centered"><thead><tr><th data-field="id">'+$scope.dias.dia1+'</th><th data-field="name">'+$scope.dias.dia2+'</th><th data-field="name">'+$scope.dias.dia3+'</th><th data-field="name">'+$scope.dias.dia4+'</th></tr></thead><tbody><tr><td>Peso Muerto</td><td>Militar</td><td>Sentadilla</td><td>Press Banca</td></tr><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td></tr>';
+      }else {
+          var cabezera = '<h3>Semana 1</h3><table class="centered"><thead><tr><th data-field="id">'+$scope.dias.dia1+'</th><th data-field="name">'+$scope.dias.dia2+'</th><th data-field="name">'+$scope.dias.dia3+'</th><th data-field="name">'+$scope.dias.dia4+'</th></tr></thead><tbody><tr><td>Militar</td><td>Peso Muerto</td><td>Press Banca</td><td>Sentadilla</td></tr><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td><td>3x5  75/80/85</td></tr>';
+      }
       var pie = '</tbody></table><h4>Accesorios</h4>'
       var contenido ='';
 
-      for (var i=0; i <4; i++) {
+      for (var i=0; i <=4; i++) {
 
-        if (i==1) {
+        if (i==0) {
           var variante=0.4;
-        }else if (i==2) {
+        }else if (i==1) {
           var variante=0.5;
-        }else if (i==3) {
+        }else if (i==2) {
           var variante=0.6;
-        }else if (true) {
+        }else if (i==3) {
           var variante=0.75;
-        }else {
+        }else if(i==4){
           var variante=0.8;
         }
 
-        var uno = $scope.mi1rm.uno*variante*0.95;
-        uno = uno.toFixed(2);
-        uno = Math.round(uno);
-        var dos = $scope.mi1rm.dos*variante*0.95;
-        dos = dos.toFixed(2);
-        dos = Math.round(dos);
-        var tres = $scope.mi1rm.tres*variante*0.95;
-        tres = tres.toFixed(2);
-        tres = Math.round(tres);
-        var cuatro = $scope.mi1rm.cuatro*variante*0.95;
-        cuatro = cuatro.toFixed(2);
-        cuatro = Math.round(cuatro);
+        if ($scope.diaEjercicio.dia1=='Pierna') {
+          var uno = $scope.mi1rm.dos*variante*0.95;
+          uno = uno.toFixed(1);
+          uno = Math.round(uno);
+          uno = RedondeoDecenas(uno);
+
+          var dos = $scope.mi1rm.uno*variante*0.95;
+          dos = dos.toFixed(2);
+          dos = Math.round(dos);
+          dos = RedondeoDecenas(dos);
+
+          var tres = $scope.mi1rm.cuatro*variante*0.95;
+          tres = tres.toFixed(2);
+          tres = Math.round(tres);
+          tres = RedondeoDecenas(tres);
+
+          var cuatro = $scope.mi1rm.tres*variante*0.95;
+          cuatro = cuatro.toFixed(2);
+          cuatro = Math.round(cuatro);
+          cuatro = RedondeoDecenas(cuatro);
+
+        }else {
+          var uno = $scope.mi1rm.uno*variante*0.95;
+          uno = uno.toFixed(1);
+          uno = Math.round(uno);
+          uno = RedondeoDecenas(uno);
+
+          var dos = $scope.mi1rm.dos*variante*0.95;
+          dos = dos.toFixed(2);
+          dos = Math.round(dos);
+          dos = RedondeoDecenas(dos);
+
+          var tres = $scope.mi1rm.tres*variante*0.95;
+          tres = tres.toFixed(2);
+          tres = Math.round(tres);
+          tres = RedondeoDecenas(tres);
+
+          var cuatro = $scope.mi1rm.cuatro*variante*0.95;
+          cuatro = cuatro.toFixed(2);
+          cuatro = Math.round(cuatro);
+          cuatro = RedondeoDecenas(cuatro);
+
+        }
 
         contenido+='<tr><td>5x'+uno+'</td><td>5x'+dos+'</td><td>5x'+tres+'</td><td>5x'+cuatro+'</td></tr>';
       }
 
 
-      var uno = $scope.mi1rm.uno*0.85*0.95;
-      uno = uno.toFixed(2);
-      uno = Math.round(uno);
-      var dos = $scope.mi1rm.dos*0.85*0.95;
-      dos = dos.toFixed(2);
-      dos = Math.round(dos);
-      var tres = $scope.mi1rm.tres*0.85*0.95;
-      tres = tres.toFixed(2);
-      tres = Math.round(tres);
-      var cuatro = $scope.mi1rm.cuatro*0.85*0.95;
-      cuatro = cuatro.toFixed(2);
-      cuatro = Math.round(cuatro);
+      if ($scope.diaEjercicio.dia1=='Pierna') {
+
+        var uno = $scope.mi1rm.dos*0.85*0.95;
+        uno = uno.toFixed(2);
+        uno = Math.round(uno);
+        uno = RedondeoDecenas(uno);
+
+        var dos = $scope.mi1rm.uno*0.85*0.95;
+        dos = dos.toFixed(2);
+        dos = Math.round(dos);
+        dos = RedondeoDecenas(dos);
+
+        var tres = $scope.mi1rm.cuatro*0.85*0.95;
+        tres = tres.toFixed(2);
+        tres = Math.round(tres);
+        tres = RedondeoDecenas(tres);
+
+        var cuatro = $scope.mi1rm.tres*0.85*0.95;
+        cuatro = cuatro.toFixed(2);
+        cuatro = Math.round(cuatro);
+        cuatro = RedondeoDecenas(cuatro);
+
+      }else {
+        var uno = $scope.mi1rm.uno*0.85*0.95;
+        uno = uno.toFixed(2);
+        uno = Math.round(uno);
+        uno = RedondeoDecenas(uno);
+
+        var dos = $scope.mi1rm.dos*0.85*0.95;
+        dos = dos.toFixed(2);
+        dos = Math.round(dos);
+        dos = RedondeoDecenas(dos);
+
+        var tres = $scope.mi1rm.tres*0.85*0.95;
+        tres = tres.toFixed(2);
+        tres = Math.round(tres);
+        tres = RedondeoDecenas(tres);
+
+        var cuatro = $scope.mi1rm.cuatro*0.85*0.95;
+        cuatro = cuatro.toFixed(2);
+        cuatro = Math.round(cuatro);
+        cuatro = RedondeoDecenas(cuatro);
+
+      }
 
       contenido+='<tr><td>>5x'+uno+'</td><td>>5x'+dos+'</td><td>>5x'+tres+'</td><td>>5x'+cuatro+'</td></tr>';
 
+      if ($scope.diaEjercicio.dia1=='Pierna') {
+        var cabezera2 = '<table class="centered"><thead><tr><th data-field="id">'+$scope.dias.dia1+'</th><th data-field="name">'+$scope.dias.dia2+'</th><th data-field="name">'+$scope.dias.dia3+'</th><th data-field="name">'+$scope.dias.dia4+'</th></tr></thead><tbody><tr><td>Sentadilla</td><td>Press Banca</td><td>Peso Muerto</td><td>Militar</td></tr><tr><td>5x10 60%</td><td>5x10 60%</td><td>5x10 60%</td><td>5x10 60%</td></tr>';
+      }else {
+          var cabezera2 = '<table class="centered"><thead><tr><th data-field="id">'+$scope.dias.dia1+'</th><th data-field="name">'+$scope.dias.dia2+'</th><th data-field="name">'+$scope.dias.dia3+'</th><th data-field="name">'+$scope.dias.dia4+'</th></tr></thead><tbody><tr><td>Press Banca</td><td>Sentadilla</td><td>Militar</td><td>Peso Muerto</td></tr><tr><td>5x10 60%</td><td>5x10 60%</td><td>5x10 60%</td><td>5x10 60%</td></tr>';
+      }
 
-      var cabezera2 = '<h3>Semana 1</h3><table class="centered"><thead><tr><th data-field="id">Lunes</th><th data-field="name">Martes</th><th data-field="name">Miercoles</th><th data-field="name">Jueves</th></tr></thead><tbody><tr><td>Militar</td><td>Peso Muerto</td><td>Press Banca</td><td>Sentadilla</td></tr>';
       var pie2 = '</tbody></table>'
       var contenido2 ='';
 
+      //For de Accesorios
+      variante = 0.6;
+      for (var i=0; i <5; i++) {
 
-      $("#camino").append(cabezera+contenido+pie);
+        if ($scope.diaEjercicio.dia1=='Pierna') {
+          var uno = $scope.mi1rm.dos*variante*0.95;
+          uno = uno.toFixed(2);
+          uno = Math.round(uno);
+          uno = RedondeoDecenas(uno);
+
+          var dos = $scope.mi1rm.uno*variante*0.95;
+          dos = dos.toFixed(2);
+          dos = Math.round(dos);
+          dos = RedondeoDecenas(dos);
+
+          var tres = $scope.mi1rm.cuatro*variante*0.95;
+          tres = tres.toFixed(2);
+          tres = Math.round(tres);
+          tres = RedondeoDecenas(tres);
+
+          var cuatro = $scope.mi1rm.tres*variante*0.95;
+          cuatro = cuatro.toFixed(2);
+          cuatro = Math.round(cuatro);
+          cuatro = RedondeoDecenas(cuatro);
+        }else {
+          var uno = $scope.mi1rm.uno*variante*0.95;
+          uno = uno.toFixed(2);
+          uno = Math.round(uno);
+          uno = RedondeoDecenas(uno);
+
+          var dos = $scope.mi1rm.dos*variante*0.95;
+          dos = dos.toFixed(2);
+          dos = Math.round(dos);
+          dos = RedondeoDecenas(dos);
+
+          var tres = $scope.mi1rm.tres*variante*0.95;
+          tres = tres.toFixed(2);
+          tres = Math.round(tres);
+          tres = RedondeoDecenas(tres);
+
+          var cuatro = $scope.mi1rm.cuatro*variante*0.95;
+          cuatro = cuatro.toFixed(2);
+          cuatro = Math.round(cuatro);
+          cuatro = RedondeoDecenas(cuatro);
+        }
+
+        contenido2+='<tr><td>10x'+uno+'</td><td>10x'+dos+'</td><td>10x'+tres+'</td><td>10x'+cuatro+'</td></tr>';
+      }
+      pie2+='<br><br><br><div class="divider"></div><table class="bordered"><tbody><tr><td><strong>DIPS- <font color="red">Progresión</font></strong></td><td><strong>Dominadas- <font color="red">Prog</font></strong></td><td><strong>DIPS- <font color="red">Progresión</font></strong></td><td><strong>Dominadas- <font color="red">Prog</font></strong></td></tr><tr><td><strong>Lat. Raises 3x10</strong></td><td><strong>Prensa 3x15</strong></td><td><strong>Press c/min 3x10</strong></td><td><strong>Ext. Cuadrice ps 3x15</strong></td></tr><tr><td><strong>P. Frances 3x10</strong></td><td><strong>Curl Femoral 3x15</strong></td><td><strong>Triceps polea 3x10</strong></td><td><strong>Curl Femoral 3x15</strong></td></tr><tr><td><strong>Curl Bece ps Ez 3x10</strong></td><td><strong>Core</strong></td><td><strong>Curl martillo 3x10</strong></td><td><strong>Core</strong></td></tr></tbody></table>'
+      $("#camino").append(cabezera+contenido+pie+cabezera2+contenido2+pie2);
     }else {
+      //Aqui lo de la pagina inicial
       document.getElementById('camino').innerHTML='';
-      $("#camino").append('<br><br><br><a href="inicio.html" class="waves-effect waves-light btn-large">Empezar</a>');
+      $("#camino").append('<br><img class="circle responsive-img" src="img/Entre1.png" width="100%" height="100%"><br><br><br><p>GymCoach es una app que te guiara el el GYM, ofreciendo mejoras en muy poco tiempo.</p><a href="inicio.html" class="waves-effect waves-light btn-large">Iniciar</a>');
     }
+
+    //fechas();
   };
 
   $scope.Reiniciar = function () {
@@ -86,9 +269,33 @@ app.controller('procedimientoController', function($scope, $http){
     );
   }
 
+  $scope.Cerrar = function() {
+    navigator.app.exitApp();
+  }
+
+  $scope.Progresion= function () {
+    if(typeof(Storage) !== "undefined"){
+      if (localStorage.getItem("mi1rm")!=null) {
+        localStorage.removeItem("mi1rm");
+      }
+        localStorage.setItem("mi1rm", JSON.stringify(
+          {'uno':$scope.mi1rm.uno+$scope.progresion1,'dos':$scope.mi1rm.dos+$scope.progresion2 ,'tres':$scope.mi1rm.tres+ $scope.progresion1,'cuatro': $scope.mi1rm.cuatro+$scope.progresion2 }));
+      }else{
+        console.log("No se pudo guardar la información en memoria");
+      }
+    $('#modal1').closeModal();
+
+  }
+
   //funcion collback de la alerta
-  function alertDismised() {
-    console.log("You selected button number " + results.buttonIndex + " and entered " + results.input1);
+  function alertDismised(botton) {
+    if (botton==1) {
+      localStorage.clear();
+      window.location.href = 'index.html';
+      //location.reload();
+      //alert("Preciono Aceptar");
+    }
+    //console.log("You selected button number " + results.buttonIndex + " and entered " + results.input1);
   }
 
 });
